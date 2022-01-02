7ada960fd3a2c5a8d99178b691c616b11a194dca
Unnathy Nellutla <unnathy5108@MacBook-Pro-88.fios-router.home>
2022-01-02 18:38:32 -0500

    celery test 8

--- mysite/celery.py
+++ mysite/celery.py
@@ -1,7 +1,11 @@
 from __future__ import absolute_import
 import os
 from celery import Celery
+from django.contrib.auth.models import User
 from django.conf import settings
+from .settings import EMAIL_HOST_USER
+from django.core.mail import send_mail
+from datetime import date, timedelta
 
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
@@ -20,3 +24,26 @@ def debug_task(self):
 @app.task
 def add(x, y):
     return x + y
+
+@app.task
+def daily_emails():
+    send = False
+    receivers = User.objects.all()
+    current_day = date.today()
+    for receiver in receivers:
+        subject= "Today's Job Alerts for " + receiver.username
+        message = 'Your deadlines in the next 24 hours: '
+        for stage in Stage.objects.filter(author=receiver): 
+            for posting in stage.ordered_posting_set().filter(deadline__day=current_day):
+                send = True
+                message += posting.job_title 
+                message += ' '
+                message += stage.stage_title
+                message += ' due on: '
+                message += posting.deadline.strftime("%m/%d/%Y, %H:%M:%S")
+                message += ' '
+            message += '.'
+        if send == True:
+            send_mail(subject,message,EMAIL_HOST_USER,[receiver.email], fail_silently= False)
+            send = False
+    return('first_task_done')
