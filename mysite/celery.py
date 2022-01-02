294cac7c69e37a16a9d598a6a2cd53e97fdcc691
Unnathy Nellutla <unnathy5108@MacBook-Pro-88.fios-router.home>
2022-01-02 18:23:36 -0500

    celery test 5

--- mysite/celery.py
+++ mysite/celery.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import
 import os
 from celery import Celery
+from jobboard.models import User
 from django.conf import settings
 
 # set the default Django settings module for the 'celery' program.
