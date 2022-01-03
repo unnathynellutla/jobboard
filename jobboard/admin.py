c5befbc6c93782df6ce7bd70b3026114d70a173f
Unnathy Nellutla <unnathy5108@MacBook-Pro-88.fios-router.home>
2022-01-02 22:06:10 -0500

    heroku scheduler 10

--- jobboard/admin.py
+++ jobboard/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Stage, Posting
+from .models import Stage, Posting, UpdateTime
 
 admin.site.register(Stage)
 admin.site.register(Posting)
+admin.site.register(UpdateTime)
--- mysite/management/commands/email.py
+++ mysite/management/commands/email.py
@@ -2,7 +2,7 @@ from django.core.management.base import BaseCommand, CommandError
 from mysite.settings import EMAIL_HOST_USER
 from django.core.mail import send_mail
 import datetime
-from jobboard.models import Posting, Stage
+from jobboard.models import Posting, Stage, UpdateTime
 from django.contrib.auth.models import User
 import pytz
 
@@ -12,13 +12,14 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         send = False
-        receivers = User.objects.all()
+        receivers = UpdateTime.values_list('comment_id', flat=True)
         current_day = datetime.datetime.now().replace(tzinfo=pytz.UTC)
         for receiver in receivers:
             subject= "Today's Job Alerts for " + receiver.username
             message = 'Your deadlines in the next 24 hours: '
             for stage in Stage.objects.filter(author=receiver): 
                 for posting in stage.ordered_posting_set():
+                    if posting.deadline- datetime.timedelta(hours=24) <= current_day <= posting.deadline:
                         send = True
                         message += posting.job_title 
                         message += ' '
