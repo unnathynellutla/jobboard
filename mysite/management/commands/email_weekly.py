7312705ca3cba6e5268b51371f84e6fb9f187db9
Unnathy Nellutla <unnathy5108@MacBook-Pro-88.fios-router.home>
2022-01-02 22:11:46 -0500

    heroku scheduler 12

---
+++ mysite/management/commands/email_weekly.py
@@ -0,0 +1,34 @@
+from django.core.management.base import BaseCommand, CommandError
+from mysite.settings import EMAIL_HOST_USER
+from django.core.mail import send_mail
+import datetime
+from jobboard.models import Posting, Stage, UpdateTime
+from django.contrib.auth.models import User
+import pytz
+
+
+class Command(BaseCommand):
+    help = 'Sends an Email'
+
+    def handle(self, *args, **options):
+        send = False
+        receivers = UpdateTime.objects.values_list('author', flat=True)
+        current_day = datetime.datetime.now().replace(tzinfo=pytz.UTC)
+        for receiver in receivers:
+            subject= "Today's Job Alerts for " + receiver.username
+            message = 'Your deadlines in the next 24 hours: '
+            for stage in Stage.objects.filter(author=receiver): 
+                for posting in stage.ordered_posting_set():
+                   # if posting.deadline-datetime.timedelta(days=7) <= current_day <= posting.deadline:
+                        send = True
+                        message += posting.job_title 
+                        message += ' '
+                        message += stage.stage_title
+                        message += ' due on: '
+                        message += posting.deadline.strftime("%m/%d/%Y, %H:%M:%S")
+                        message += ' '
+            if send == True:
+                message += '.'
+                send_mail(subject,message,EMAIL_HOST_USER,[receiver.email], fail_silently= False)
+                send = False
+        return('first_task_done')
