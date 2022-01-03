cf0723f5861f987e1cacd2e574058e994e762be5
Unnathy Nellutla <unnathy5108@MacBook-Pro-88.fios-router.home>
2022-01-02 19:05:25 -0500

    celery test 12

--- mysite/celery.py
+++ mysite/celery.py
@@ -19,15 +19,7 @@ app.autodiscover_tasks()
 @app.on_after_configure.connect
 def setup_periodic_tasks(sender, **kwargs):
     # Calls test('hello') every 10 seconds.
-    sender.add_periodic_task(10.0, test.s('hello'), name='add every 10')
-
-    # Calls test('world') every 30 seconds
-    sender.add_periodic_task(30.0, test.s('world'), expires=10)
-
-    # Executes every Monday morning at 7:30 a.m.
-    sender.add_periodic_task(
-        crontab(hour=7, minute=30, day_of_week=1),
-        test.s('Happy Mondays!'),
+    sender.add_periodic_task(10.0, daily_emails(), name='add every 10')
 )
 
 @app.task(bind=True)
