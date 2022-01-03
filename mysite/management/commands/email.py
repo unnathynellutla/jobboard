05e10eca1e12a4135d8afb1b33ebce4c9343f3fa
Unnathy Nellutla <unnathy5108@MacBook-Pro-88.fios-router.home>
2022-01-02 21:06:23 -0500

    switch to heroku scheduler 10

--- mysite/management/commands/email.py
+++ mysite/management/commands/email.py
@@ -3,15 +3,30 @@ from mysite.settings import EMAIL_HOST_USER
 from django.core.mail import send_mail
 from datetime import date, timedelta
 from jobboard.models import Posting, Stage
+from django.contrib.auth.models import User
 
 
 class Command(BaseCommand):
     help = 'Sends an Email'
 
     def handle(self, *args, **options):
-        receivers = "unnathy.nellutla@tufts.edu"
+        send = False
+        receivers = User.objects.all()
         current_day = date.today()
-        subject= "Today's Job Alerts for "
-        message = 'Your deadlines in the next 24 hours: '
-        send_mail(subject,message,EMAIL_HOST_USER,[receivers], fail_silently= False)
+        for receiver in receivers:
+            subject= "Today's Job Alerts for " + receiver.username
+            message = 'Your deadlines in the next 24 hours: '
+            for stage in Stage.objects.filter(author=receiver): 
+                for posting in stage.ordered_posting_set().filter(deadline__day=current_day):
+                    send = True
+                    message += posting.job_title 
+                    message += ' '
+                    message += stage.stage_title
+                    message += ' due on: '
+                    message += posting.deadline.strftime("%m/%d/%Y, %H:%M:%S")
+                    message += ' '
+                message += '.'
+            if send == True:
+                send_mail(subject,message,EMAIL_HOST_USER,[receiver.email], fail_silently= False)
+                send = False
         return('first_task_done')
