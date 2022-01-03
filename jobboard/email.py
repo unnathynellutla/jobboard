790b6001d8f5514fffc6cf22806ef673601c7dcc
Unnathy Nellutla <unnathy5108@MacBook-Pro-88.fios-router.home>
2022-01-02 20:00:59 -0500

    switch to heroku scheduler

---
+++ jobboard/email.py
@@ -0,0 +1,15 @@
+from django.core.management.base import BaseCommand, CommandError
+from mysite.settings import EMAIL_HOST_USER
+from django.core.mail import send_mail
+from datetime import date, timedelta
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        receivers = "unnathy.nellutla@tufts.edu"
+        current_day = date.today()
+        subject= "Today's Job Alerts for "
+        message = 'Your deadlines in the next 24 hours: '
+        send_mail(subject,message,EMAIL_HOST_USER,[receivers], fail_silently= False)
+        return('first_task_done')
